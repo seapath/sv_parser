94c245bebf30d86cbf03c9a7c601d2348a57da45
Benjamin Chedotel <benjamin.chedotel@savoirfairelinux.com>
2024-03-26 09:44:36 +0100

    sv_parser: add error code, change structure name and change types
    Add BAD_FORMAT error code and change structure name
    to be more readable. Change APPID and length size because they are on
    2 bytes.
    
    Change-Id: I187f47b525b50036edd8015f3a89ed626eceb4ad
    Signed-off-by: Benjamin Chedotel <benjamin.chedotel@savoirfairelinux.com>

--- sv_parser.h
+++ sv_parser.h
@@ -9,11 +9,13 @@
 
 #include <stdint.h>
 
+#define BAD_FORMAT -1 /**< Error code when using a wrong sv payload */
+
 /**
  * \struct SV_ASDU
  * \brief Sample Value Application Specific Data Unit
  * */
-typedef struct {
+struct SV_ASDU {
     char* svId; /**< User-defined unique string identifier used
                   for subscription */
     uint8_t* smpCnt; /**< Index of the SV message */
@@ -24,16 +26,29 @@ typedef struct {
     int seqDataLength; /**< Length of the sequence of data */
     uint8_t* seqDataAddr; /**< Sequence of measured voltage and
                             current values */
-} SV_ASDU;
+};
 
 /**
  * \struct SV_payload
  * \brief Sample Value payload
  * */
-typedef struct {
-   uint8_t* APPID; /**< Application ID */
-   uint8_t* length; /**< Message length */
+struct SV_payload {
+   uint16_t APPID; /**< Application ID */
+   uint16_t length; /**< Message length */
 
    uint8_t noASDU; /**< Number of ASDU in the sequence */
-   SV_ASDU* seqASDU; /**< Sequence of ASDU */
-} SV_payload;
+   struct SV_ASDU* seqASDU; /**< Sequence of ASDU */
+};
+
+// constructor & destructor
+struct SV_payload* create_SV();
+void free_SV(struct SV_payload *sv);
+
+// constructor & destructor
+struct SV_payload* create_SV();
+void free_SV(struct SV_payload *sv);
+
+// parsing functions
+int parse_SV_ASDU(const uint8_t *rawData, int rawDataLength,
+                  struct SV_ASDU *asdu);
+int parse_SV_payload(const uint8_t *payload, struct SV_payload *sv);
