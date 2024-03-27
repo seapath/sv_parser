343c1234a287a0cd15aa04b665b48417ce8abe4d
Benjamin Chedotel <benjamin.chedotel@savoirfairelinux.com>
2024-03-27 10:51:09 +0100

    sv_parser: edit data structures
    Edit data structures SV_payload and SV_ASDU to add new element and
    change types.
    
    Change-Id: Idc0d3e9714333e845808e1ec1edb4344db9a96a3
    Signed-off-by: Benjamin Chedotel <benjamin.chedotel@savoirfairelinux.com>

--- sv_parser.h
+++ sv_parser.h
@@ -16,15 +16,21 @@
  * \brief Sample Value Application Specific Data Unit
  * */
 struct SV_ASDU {
-    char* svId; /**< User-defined unique string identifier used
+        char *svID; /**< User-defined unique string identifier used
                   for subscription */
-    uint8_t* smpCnt; /**< Index of the SV message */
-    uint8_t* confRef; /**< Configuration revision */
-    uint8_t* smpSynch; /**< Synchronization mechanism of the clock used
-                         for sending the SV */
+        char *datSet; /**< The name of the data set being sent */
 
-    int seqDataLength; /**< Length of the sequence of data */
-    uint8_t* seqDataAddr; /**< Sequence of measured voltage and
+        uint16_t smpCnt; /**< Index of the SV message */
+        uint32_t confRev; /**< Configuration revision */
+        uint8_t smpSynch; /**< Synchronization mechanism of the clock used
+                         for sending the SV */
+        uint8_t smpMod; /**< Sample Mode :samples/period, sampl/sec,
+                          sec/sampl. */
+        uint16_t smpRate; /**< Sample rate */
+        uint64_t refrTm; /**< Time of the first sample */
+
+        int seqDataLength; /**< Length of the sequence of data */
+        uint8_t *seqData; /**< Sequence of measured voltage and
                             current values */
 };
 
@@ -33,11 +39,11 @@ struct SV_ASDU {
  * \brief Sample Value payload
  * */
 struct SV_payload {
-   uint16_t APPID; /**< Application ID */
-   uint16_t length; /**< Message length */
+        uint16_t APPID; /**< Application ID */
+        uint16_t length; /**< Message length */
 
-   uint8_t noASDU; /**< Number of ASDU in the sequence */
-   struct SV_ASDU* seqASDU; /**< Sequence of ASDU */
+        uint8_t noASDU; /**< Number of ASDU in the sequence */
+        struct SV_ASDU *seqASDU; /**< Sequence of ASDU */
 };
 
 // constructor & destructor
