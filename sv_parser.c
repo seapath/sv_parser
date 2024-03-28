3cb28eb0a7f3351f460745bb539fee0890e0fd3c
Benjamin Chedotel <benjamin.chedotel@savoirfairelinux.com>
2024-03-28 12:13:44 +0100

    sv_parser: add SV_payload constructor
    Change-Id: I7971cfe7b61152919ec51ffb90cb250ae32b4aaf
    Signed-off-by: Benjamin Chedotel <benjamin.chedotel@savoirfairelinux.com>

--- sv_parser.c
+++ sv_parser.c
@@ -7,4 +7,31 @@
  *
  */
 
-#include "lib/sv_parser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sv_parser.h"
+
+/**
+ * \brief SV_payload constructor
+ *
+ * \param payload Raw data from payload
+ * \return Allocated structure
+ * */
+struct SV_payload* create_SV()
+{
+        struct SV_payload *sv;
+
+        sv = malloc(sizeof(struct SV_payload));
+
+        /* allocate ASDU */
+        sv->seqASDU = malloc(8 * sizeof(struct SV_ASDU)); // max 8 ASDU
+        memset(sv->seqASDU, 0, 8*sizeof(struct SV_ASDU));
+        for(int i=0; i<8; ++i) {
+                sv->seqASDU[i].svID = malloc(34 * sizeof(char)); // svID max
+                sv->seqASDU[i].datSet = malloc(130 * sizeof(char)); // datSet
+                                                                    // max
+                sv->seqASDU[i].seqData = malloc(64 * sizeof(uint8_t));
+        }
+        return sv;
+}
